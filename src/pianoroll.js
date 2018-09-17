f99f1804e4a9f48c649d16d85563d929c76893ea
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-17 04:57:31 +0200

    pianoroll: second draft

--- src/pianoroll.js
+++ src/pianoroll.js
@@ -1,23 +1,30 @@
 "use strict";
 
-class Pianoroll {
-	constructor() {
+DAW.Pianoroll = class {
+	constructor( ctx ) {
+		const waSched = new gswaScheduler(),
+			waSynth = new gswaSynth();
+
 		this.keys = {};
-		this.synth = null;
+		this.synth = {};
 		this.playing = false;
 		this.currentTime = 0;
-		this._sched = new gswaScheduler();
+		this._ctx = ctx;
+		this._waSched = waSched;
+		this._waSynth = waSynth;
+		this._keysStarted = {};
+		this._keysStartedLive = {};
+		waSched.currentTime = () => this._ctx.currentTime;
+		waSched.ondatastart = this._startKey.bind( this );
+		waSched.ondatastop = this._stopKey.bind( this );
 	}
 
-	// load
-	// ........................................................................
-	load( keys ) {
-		this.empty();
+	loadKeys( keys ) {
 		// ...
 	}
-
-	// empty
-	// ........................................................................
+	loadSynth( syn ) {
+		// ...
+	}
 	empty() {
 		this.stop();
 		if ( this.keys ) {
@@ -25,35 +32,51 @@ class Pianoroll {
 		}
 	}
 
+	// ........................................................................
+	_startKey( startedId, blc, when, off, dur ) {
+		this._keysStarted[ startedId ] =
+			this._waSynth.startKey( blc.key, when, off, dur, blc.gain, blc.pan );
+	}
+	_stopKey( startedId, blc ) {
+		this._waSynth.stopKey( this._keysStarted[ startedId ] );
+		delete this._keysStarted[ startedId ];
+	}
+
 	// controls
 	// ........................................................................
 	setCurrentTime( t ) {
 		this.currentTime = t;
-		this._sched.setCurrentOffset( t );
+		this._waSched.setCurrentOffset( t );
 	}
 	setLoop( a, b ) {
-		this._sched.setLoop( a, b );
+		this._waSched.setLoop( a, b );
 	}
 	setBPM( bpm ) {
-		this._sched.setBPM( bpm );
+		this._waSched.setBPM( bpm );
 	}
 	liveKeydown( midi ) {
-
+		if ( !( midi in this._keysStartedLive ) ) {
+			this._keysStartedLive[ midi ] = this._waSynth.startKey(
+				midi, this._waSched.currentTime(), 0, Infinity, .8, 0 );
+		}
 	}
 	liveKeyup( midi ) {
-
+		if ( this._keysStartedLive[ midi ] ) {
+			this._waSynth.stopKey( this._keysStartedLive[ midi ] );
+			delete this._keysStartedLive[ midi ];
+		}
 	}
 	play() {
 		if ( !this.playing ) {
-			this._sched.start( 0, this.currentTime );
+			this._waSched.start( 0, this.currentTime );
 			this.playing = true;
 		}
 	}
 	pause() {
 		if ( this.playing ) {
 			this.playing = false;
-			this.currentTime = this._sched.getCurrentOffset();
-			this._sched.stop();
+			this.currentTime = this._waSched.getCurrentOffset();
+			this._waSched.stop();
 		}
 	}
 	stop() {
@@ -63,4 +86,4 @@ class Pianoroll {
 			this.currentTime = 0;
 		}
 	}
-}
+};
