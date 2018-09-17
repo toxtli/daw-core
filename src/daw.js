1a85fc4d5806df115a4eb7367a6de38818a03393
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-17 04:55:37 +0200

    daw: call(), use a namespace for all the callbacks

--- src/daw.js
+++ src/daw.js
@@ -2,19 +2,20 @@
 
 class DAW {
 	constructor() {
+		this.cb = {};
 		this.cmp =
 		this.cmpId =
 		this.synthOpened =
 		this.patternOpened = null;
 		this.compositions = new Map();
-		this.composition = new Composition();
+		this.composition = new DAW.Composition();
 		this.ctx = new AudioContext();
 	}
 
 	call( cbName, a, b, c, d ) {
-		const fn = this[ cbName ];
+		const fn = this.cb[ cbName ];
 
-		fn && fn.call( this, a, b, c, d );
+		return fn && fn( a, b, c, d );
 	}
 	initPianoroll() {
 		this.pianoroll = new DAW.Pianoroll( this.ctx );
