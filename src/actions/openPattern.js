d4cd3c8b49d78b9c26765b816c4847d4cb880eac
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-21 01:50:58 +0200

    openSynth/Pattern(): rewrite, use the json conception

--- src/actions/openPattern.js
+++ src/actions/openPattern.js
@@ -1,30 +1,15 @@
 "use strict";
 
 DAW.prototype.openPattern = function( id ) {
-	if ( id !== this.patternOpened ) {
-		const cmp = this.cmp,
-			pat = cmp.patterns[ id ],
-			synId = pat.synth,
-			synOpened = this.synthOpened,
-			patOpened = this.patternOpened;
+	const cmp = this.cmp;
 
-		this.pianoroll.loadKeys( cmp.keys[ pat.keys ] );
-		this.patternOpened =
-		cmp.patternOpened = id;
-		if ( synId !== synOpened ) {
-			const syn = cmp.synths[ synId ];
+	if ( id !== cmp.patternOpened ) {
+		const synId = cmp.patterns[ id ].synth,
+			obj = { patternOpened: id };
 
-			this.pianoroll.loadSynth( syn );
-			this.synthOpened =
-			cmp.synthOpened = synId;
-			if ( synOpened ) {
-				this.call( "synthesizerClosed", synOpened );
-			}
-			this.call( "synthesizerOpened", synId, syn );
+		if ( synId !== cmp.synthOpened ) {
+			obj.synthOpened = synId;
 		}
-		if ( patOpened ) {
-			this.call( "patternClosed", patOpened );
-		}
-		this.call( "patternOpened", id, pat );
+		this.composition.change( obj );
 	}
 };
--- src/actions/openSynth.js
+++ src/actions/openSynth.js
@@ -1,35 +1,22 @@
 "use strict";
 
 DAW.prototype.openSynth = function( id ) {
-	if ( id !== this.synthOpened ) {
-		const cmp = this.cmp,
-			syn = cmp.synths[ id ],
-			synOpened = this.synthOpened,
-			patOpened = this.patternOpened;
+	const cmp = this.cmp;
 
-		this.pianoroll.loadSynth( syn );
-		this.synthOpened =
-		cmp.synthOpened = id;
-		if ( synOpened ) {
-			this.call( "synthesizerClosed", synOpened );
+	if ( id !== cmp.synthOpened ) {
+		const patId = this._openSynth_find( cmp, id ),
+			obj = { synthOpened: id };
+
+		if ( patId !== cmp.patternOpened ) {
+			obj.patternOpened = patId;
 		}
-		this.call( "synthesizerOpened", id, syn );
-		if ( !Object.entries( cmp.patterns ).some( ( [ patId, pat ] ) => {
-			if ( pat.synth === id ) {
-				this.pianoroll.loadKeys( cmp.keys[ pat.keys ] );
-				this.patternOpened =
-				cmp.patternOpened = patId;
-				if ( patOpened ) {
-					this.call( "patternClosed", patOpened );
-				}
-				this.call( "patternOpened", patId, pat );
-				return true;
-			}
-		} ) && patOpened ) {
-			this.pianoroll.empty();
-			this.patternOpened =
-			cmp.patternOpened = null;
-			this.call( "patternClosed", patOpened );
-		}
+		this.composition.change( obj );
 	}
+};
+
+DAW.prototype._openSynth_find = function( cmp, id ) {
+	const pat = Object.entries( cmp.patterns )
+			.find( ( [ patId, pat ] ) => pat.synth === id );
+
+	return pat ? pat[ 0 ] : null;
 };
