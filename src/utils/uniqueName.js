2869c58007aef310748a4a396d8192a7e06897ba
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-21 01:48:01 +0200

    daw.uniqueName

---
+++ src/utils/uniqueName.js
@@ -0,0 +1,20 @@
+"use strict";
+
+DAW.uniqueName = ( nameOri, arr ) => {
+	const name = DAW.trim2( nameOri );
+
+	if ( arr.indexOf( name ) > -1 ) {
+		const name2 = /-\d+$/.test( name )
+				? name.substr( 0, name.lastIndexOf( "-" ) ).trim()
+				: name,
+			reg = new RegExp( "^" + name2 + "-(\\d+)$" ),
+			nb = arr.reduce( ( nb, str ) => {
+				const res = reg.exec( str );
+
+				return res ? Math.max( nb, +res[ 1 ] ) : nb;
+			}, 1 );
+
+		return `${ name }-${ nb + 1 }`;
+	}
+	return name;
+};
