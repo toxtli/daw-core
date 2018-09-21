796abe604f8106150931c54c5f5f2be64ca7a93f
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-21 02:30:32 +0200

    newSynth

---
+++ src/actions/newSynth.js
@@ -0,0 +1,24 @@
+"use strict";
+
+DAW.prototype.newSynth = function() {
+	this.history.stackChange( this.composition.change( this._newSynth() ) );
+};
+
+DAW.prototype._newSynth = function() {
+	return {
+		synths: {
+			[ this._getMaxIdOf( this._cmp.synths ) ]: {
+				name: this._createUniqueName( "synths", "synth" ),
+				oscillators: {
+					"0": {
+						order: 0,
+						type: "sine",
+						detune: 0,
+						pan: 0,
+						gain: 1,
+					}
+				}
+			}
+		}
+	};
+};
