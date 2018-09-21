2f84dd5b1ead4b04739d2f2dd080f2d235df5e1e
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-21 02:31:09 +0200

    removePattern

---
+++ src/actions/removePattern.js
@@ -0,0 +1,29 @@
+"use strict";
+
+DAW.prototype.removePattern = function( id ) {
+	const pat = this._getObjFromComposition( "patterns", id );
+
+	!pat
+		? this._error( "removePattern", "patterns", id )
+		: this.history.stackChange( this.composition.change(
+			this._removePattern( id, pat ) ) );
+};
+
+DAW.prototype._removePattern = function( patId, pat ) {
+	const cmp = this._cmp,
+		obj = {
+			keys: { [ pat.keys ]: null },
+			patterns: { [ patId ]: null },
+		},
+		blocks = Object.entries( cmp.blocks ).reduce( ( blocks, [ blcId, blc ] ) => {
+			if ( blc.pattern === patId ) {
+				blocks[ blcId ] = null;
+			}
+			return blocks;
+		}, {} );
+
+	if ( DAW.objectIsEmpty( blocks ) ) {
+		obj.blocks = blocks;
+	}
+	return obj;
+};
