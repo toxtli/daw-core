2cbefba8d903246240aba796bc3b42b5a87eabc5
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-21 06:21:55 +0200

    newComposition

---
+++ src/actions/newComposition.js
@@ -0,0 +1,45 @@
+"use strict";
+
+DAW.prototype.newComposition = function() {
+	return this.composition.load( this._newComposition() )
+		.then( cmp => this.cmp = cmp );
+};
+
+DAW.prototype._newComposition = function() {
+	const env = this.env,
+		tracks = {};
+
+	for ( let i = 0; i < env.def_nbTracks; ++i ) {
+		tracks[ i ] = {};
+	}
+	return {
+		id: DAW.uuid(),
+		bpm: env.def_bpm,
+		stepsPerBeat: env.def_stepsPerBeat,
+		beatsPerMeasure: env.def_beatsPerMeasure,
+		name: "",
+		duration: 0,
+		loopA: false,
+		loopB: false,
+		patterns: {
+			0: {
+				name: "pat",
+				type: "keys",
+				keys: 0,
+				synth: 0,
+				duration: env.def_beatsPerMeasure
+			}
+		},
+		synths: {
+			0: {
+				name: "synth",
+				oscillators: {
+					0: { order: 0, type: "sine", detune: 0, pan: 0, gain: 1 },
+				}
+			}
+		},
+		tracks,
+		blocks: {},
+		keys: { 0: {} },
+	}
+};
