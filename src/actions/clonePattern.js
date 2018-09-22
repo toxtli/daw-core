8e00e87b481a2edcac873236b271c66ea8332192
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-23 01:25:01 +0200

    clonePattern: fix, _getMaxIdOf + 1

--- src/actions/clonePattern.js
+++ src/actions/clonePattern.js
@@ -13,8 +13,8 @@ DAW.prototype._clonePattern = function( patId, pat ) {
 	const cmp = this.cmp,
 		newPat = Object.assign( {}, pat ),
 		newKeys = DAW.deepAssign( {}, cmp.keys[ pat.keys ] ),
-		newPatId = this._getMaxIdOf( cmp.patterns ),
-		newKeysId = this._getMaxIdOf( cmp.keys );
+		newPatId = this._getMaxIdOf( cmp.patterns ) + 1,
+		newKeysId = this._getMaxIdOf( cmp.keys ) + 1;
 
 	newPat.keys = newKeysId;
 	newPat.name = this._createUniqueName( "patterns", pat.name );
