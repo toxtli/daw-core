751602e79f74311553b80cf032c337cd4594f98e
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-24 02:15:38 +0200

    Composition.epure

---
+++ src/Composition.epure.js
@@ -0,0 +1,22 @@
+"use strict";
+
+DAW.Composition.epure = function( cmp ) {
+	Object.values( cmp.tracks ).forEach( tr => {
+		if ( !tr.name ) { delete tr.name; }
+		if ( tr.toggle ) { delete tr.toggle; }
+	} );
+	Object.values( cmp.blocks ).forEach( blc => {
+		if ( !blc.offset ) { delete blc.offset; }
+		if ( !blc.selected ) { delete blc.selected; }
+		if ( !blc.durationEdited ) { delete blc.durationEdited; }
+	} );
+	Object.values( cmp.keys ).forEach( keys => (
+		Object.values( keys ).forEach( key => {
+			if ( !key.prev ) { delete key.prev; }
+			if ( !key.next ) { delete key.next; }
+			if ( !key.offset ) { delete key.offset; }
+			if ( !key.selected ) { delete key.selected; }
+		} )
+	) );
+	return cmp;
+};
