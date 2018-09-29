5017e9631908486979ea835dfb30c11461ae6d2b
Thomas Tortorini <thomastortorini@gmail.com>
2018-09-29 03:49:30 +0200

    changePatternSynth: check if the value is different

--- src/actions/changePatternSynth.js
+++ src/actions/changePatternSynth.js
@@ -3,7 +3,9 @@
 DAW.prototype.changePatternSynth = function( id, synth ) {
 	const pat = this._getObjFromComposition( "patterns", id );
 
-	!pat
-		? this._error( "changePatternSynth", "patterns", id )
-		: this.history.stackChange( { patterns: { [ id ]: { synth } } } );
+	if ( !pat ) {
+		this._error( "changePatternSynth", "patterns", id );
+	} else if ( pat.synth !== synth ) {
+		this.history.stackChange( { patterns: { [ id ]: { synth } } } );
+	}
 };
