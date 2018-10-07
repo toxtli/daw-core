bfac7dbd6a77906657280c387069a6d9d3c2f00e
Thomas Tortorini <thomastortorini@gmail.com>
2018-10-07 06:45:07 +0200

    DAW: env, add some attributes

--- src/DAW.js
+++ src/DAW.js
@@ -5,9 +5,14 @@ class DAW {
 		this.cb = {};
 		this.env = {
 			def_bpm: 120,
+			def_appGain: .5,
 			def_nbTracks: 21,
 			def_stepsPerBeat: 4,
 			def_beatsPerMeasure: 4,
+			analyserFFTsize: 1024,
+			analyserEnable: true,
+			sampleRate: 44100,
+			clockSteps: false,
 		};
 		this.cmp =
 		this.cmpId =
