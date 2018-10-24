1c793037750f2cd6f9637c1e7252dcbf176ae2f4
Thomas Tortorini <thomastortorini@gmail.com>
2018-10-24 02:33:31 +0200

    saveComposition

--- src/actions/saveComposition.js
+++ src/actions/saveComposition.js
@@ -1,7 +1,5 @@
 "use strict";
 
 DAWCore.prototype.saveComposition = function() {
-	if ( this.composition.save() ) {
-		this._call( "compositionSaved", this.get.composition() );
-	}
+	this.composition.save();
 };
