7c05e4c5ae12787fb6316dbded54337101cc46d9
Thomas Tortorini <thomastortorini@gmail.com>
2018-10-24 02:33:58 +0200

    closeComposition: call stop, currentTime to 0

--- src/actions/closeComposition.js
+++ src/actions/closeComposition.js
@@ -4,6 +4,9 @@ DAWCore.prototype.closeComposition = function() {
 	if ( this.composition.loaded ) {
 		const cmp = this.get.composition();
 
+		this.stop();
+		this.pianoroll.setCurrentTime( 0 );
+		this.composition.setCurrentTime( 0 );
 		this._stopLoop();
 		this._call( "compositionClosed", cmp );
 		this.composition.unload();
